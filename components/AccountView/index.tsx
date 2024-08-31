b4905e3f5e3ac1d962fc284104ab67df6c8e3090
Apolleon <apolleonmail@gmail.com>
2024-09-01 02:38:32 +0300

    fixed styles

--- components/AccountView/index.tsx
+++ components/AccountView/index.tsx
@@ -7,22 +7,22 @@ const AccountView: FC = () => {
 
   return (
     <div className="fixed w-full top-0 left-0 p-1 z-50 ">
-      <div className="w-full flex flex-col gap-2 bg-stone-800 p-2 rounded-md shadow-lg text-slate-200">
-        <div className="w-full flex justify-between  text-2xl">
+      <div className="w-full flex flex-col  bg-stone-800 p-2 rounded-md  text-slate-200">
+        <div className="w-full flex justify-between  text-xl">
           <span className="">{account.name}</span>
           <div className="flex items-center">
             <img src={"home/paid.svg"} alt="coin" />
             <span className="">{account.coins}</span>
           </div>
         </div>
-        <div className="w-full">
+        <div className="w-full -mt-3">
           <progress
             className="w-full bg-slate-50"
-            max={account?.level?.goal}
-            value={account.exp}
+            max={50}
+            value={20}
             style={{ boxShadow: "0 2px 5px rgba(0, 0, 0, 0.25) inset" }}
           />
-          <div className="w-full flex justify-between">
+          <div className="w-full flex justify-between text-sm">
             <span className="">
               {account?.level?.current} {dictionary[account?.locale]?.lvl}
             </span>
